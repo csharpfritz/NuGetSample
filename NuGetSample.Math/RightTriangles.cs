28061482a434f473f586ac0f050482b73b866f5a
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2015-10-26 18:54:15 -0400

    Initial commit

---
+++ NuGetSample.Math/RightTriangles.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuGetSample.Math
+{
+
+  public class RightTriangles
+  {
+
+    /// <summary>
+    /// Work with pythagorean theorem to identify the hypotenuse length of a triangle
+    /// </summary>
+    /// <param name="a">One side's length</param>
+    /// <param name="b">Second side's length</param>
+    /// <returns>Third side's length</returns>
+    public static double CalculateHypoteneuseLength(double a, double b)
+    {
+
+      if (a <= 0) throw new ArgumentOutOfRangeException(nameof(a), "Parameter must be greater than zero");
+      if (b <= 0) throw new ArgumentOutOfRangeException(nameof(b), "Parameter must be greater than zero");
+      return System.Math.Sqrt(a * a + b * b);
+
+    }
+
+  }
+
+}
---
+++ TestMath/VerifyHypotenuse.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestMath
+{
+
+  public class VerifyHypotenuse
+  {
+
+    [Fact]
+    public void ShouldCalculateNormally()
+    {
+
+      // Arrange
+      var arg1 = 3D;
+      var arg2 = 4D;
+
+      // Act
+      var result = NuGetSample.Math.RightTriangles.CalculateHypoteneuseLength(arg1, arg2);
+
+      // Assert
+      Assert.Equal(5, result);
+
+    }
+
+    [Fact]
+    public void ShouldErrorOnNonPositiveArguments()
+    {
+
+      // Arrange
+      var arg1 = -1D;
+      var arg2 = 4D;
+
+      // Act , Assert
+      Assert.Throws<ArgumentOutOfRangeException>("a", () => {
+        NuGetSample.Math.RightTriangles.CalculateHypoteneuseLength(arg1, arg2);
+      });
+        
+    }
+
+  }
+
+}
