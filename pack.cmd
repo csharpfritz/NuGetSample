d594fa0e9dc5bbb7e27db2ec0420880ae66e0aab
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2015-10-28 12:58:22 -0400

    Complete demo

--- pack.cmd
+++ pack.cmd
@@ -1,16 +1,20 @@
 @echo off
 
 if \{%1\}==\{\} goto :help
-if not exist .\bin\%1\*.dll goto :missing
+if not exist .\NuGetSample.Math.Managed\bin\%1\*.dll goto :missing
 
 :: Clean up the existing nuget working folder
 rd /s /q nuget
+md nuget\lib\net451
+md nuget\lib\wp81
 md nuget\lib\dotnet
 md dist
 
-:: Copy the DLL to the correct folder
+:: Copy the DLLs to the correct folder
 
-copy .\bin\%1\NuGetSample.Math.dll .\nuget\lib\dotnet
+copy .\NuGetSample.Math.Managed\bin\%1\NuGetSample.Math.Managed.dll .\nuget\lib\net451
+copy .\NuGetSample.Math.Phone\bin\%1\NuGetSample.Math.Phone.dll .\nuget\lib\wp81
+copy .\NuGetSample.Math.PCL\bin\%1\NuGetSample.Math.PCL.dll .\nuget\lib\dotnet
 
 :: Pack the nupkg
 nuget pack NuGetSample.Math.nuspec -BasePath nuget -OutputDirectory dist
@@ -27,3 +31,5 @@ echo Pass a configuration in: "debug" or "release" to pack that version
 echo of the project
 
 :end
+
+pause
