4f88b0939a215b06d27ae9390db4927f1b75e74f
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2015-10-29 18:22:37 -0400

    Updated for Win10

---
+++ NuGetSample.Math.ModernPCL/RightTriangles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NuGetSample.Math
+{
+
+  public class RightTriangles
+  {
+
+    /// <summary>
+    /// Work with pythagorean theorem to identify the hypotenuse length of a triangle
+    /// </summary>
+    /// <param name="a">One side's length</param>
+    /// <param name="b">Second side's length</param>
+    /// <returns>Third side's length</returns>
+    public static double CalculateHypoteneuseLength(double a, double b)
+    {
+
+      // Argument validation
+      if (a <= 0) throw new ArgumentOutOfRangeException("a", "Parameter must be greater than zero");
+      if (b <= 0) throw new ArgumentOutOfRangeException("b", "Parameter must be greater than zero");
+
+      return System.Math.Sqrt(a * a + b * b);
+
+    }
+
+  }
+
+}
--- pack.cmd
+++ pack.cmd
@@ -5,15 +5,17 @@ if not exist .\NuGetSample.Math.Managed\bin\%1\*.dll goto :missing
 
 :: Clean up the existing nuget working folder
 rd /s /q nuget
-md nuget\lib\net40
-md nuget\lib\wpa
-md nuget\lib\portable-net45+sl50+windows8
+:: md nuget\lib\net40
+:: md nuget\lib\wpa
+::md nuget\lib\portable-net45+sl50+windows8
+md nuget\lib\dotnet
 md dist
 
 :: Copy the DLLs to the correct folder
-copy .\NuGetSample.Math.Managed\bin\%1\NuGetSample.Math.Managed.dll .\nuget\lib\net40
-copy .\NuGetSample.Math.WindowsPhone\bin\%1\NuGetSample.Math.WindowsPhone.dll .\nuget\lib\wpa
-copy .\NuGetSample.Math.PCL8\bin\%1\NuGetSample.Math.PCL8.dll .\nuget\lib\"portable-net45+sl50+windows8"
+::copy .\NuGetSample.Math.Managed\bin\%1\NuGetSample.Math.Managed.dll .\nuget\lib\net40
+::copy .\NuGetSample.Math.WindowsPhone\bin\%1\NuGetSample.Math.WindowsPhone.dll .\nuget\lib\wpa
+::copy .\NuGetSample.Math.PCL8\bin\%1\NuGetSample.Math.PCL8.dll .\nuget\lib\"portable-net45+sl50+windows8"
+copy .\NuGetSample.Math.ModernPCL\bin\%1\NuGetSample.Math.ModernPCL.dll .\nuget\lib\dotnet
 
 :: Pack the nupkg
 nuget pack NuGetSample.Math.nuspec -BasePath nuget -OutputDirectory dist
