d64a3234c513b81b3f4c77ec6518e5007e6e3246
Jeffrey T. Fritz <jeffreytfritz@gmail.com>
2015-10-29 17:35:47 -0400

    Updated with test Win8 application

--- pack.cmd
+++ pack.cmd
@@ -7,11 +7,13 @@ if not exist .\NuGetSample.Math.Managed\bin\%1\*.dll goto :missing
 rd /s /q nuget
 md nuget\lib\net40
 md nuget\lib\wpa
+md nuget\lib\portable-net45+sl50+windows8
 md dist
 
 :: Copy the DLLs to the correct folder
 copy .\NuGetSample.Math.Managed\bin\%1\NuGetSample.Math.Managed.dll .\nuget\lib\net40
 copy .\NuGetSample.Math.WindowsPhone\bin\%1\NuGetSample.Math.WindowsPhone.dll .\nuget\lib\wpa
+copy .\NuGetSample.Math.PCL8\bin\%1\NuGetSample.Math.PCL8.dll .\nuget\lib\"portable-net45+sl50+windows8"
 
 :: Pack the nupkg
 nuget pack NuGetSample.Math.nuspec -BasePath nuget -OutputDirectory dist
